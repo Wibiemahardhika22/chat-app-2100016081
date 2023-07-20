a2a771f894a3a3dcc88285c0c246baabb59430de
Wibiemahardhika22 <wibie2100016081@webmail.uad.ac.id>
2023-07-20 14:26:17 +0700

    Fix Bug

--- backend/index.js
+++ backend/index.js
@@ -11,9 +11,12 @@ const { notFound, errorHandler } = require('./middlewares/errorMiddleware');
 dotenv.config();
 connectDB();
 const app = express();
-app.use(cors({
-    origin: '*',
-}));
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
 app.use(express.json());
 
 app.get('/', (req, res) => {
